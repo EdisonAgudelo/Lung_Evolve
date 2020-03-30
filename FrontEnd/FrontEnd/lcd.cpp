d838fae3e8f24dcdbae2a035e5da7fdc57a96891
alondonoborja <alejandralb0324@hotmail.com>
2020-03-29 23:01:02 -0500

    lcd initialization/set menu

---
+++ FrontEnd/FrontEnd/lcd.cpp
@@ -0,0 +1,100 @@
+
+#include <lcd.h>
+#include <LiquidCrystal.h>
+
+enum btns_mnu{
+  menu_ini = 0,
+  btnRIGTH,
+  btnUP,
+  btnDOWN,
+  btnLEFT,
+  btnSELECT,
+  btnNONE
+};
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7); 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 2, 7, 8, 9, 10);
+
+void init_lcd(void){
+ 
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+
+  int lcd_key = 0;
+  int adc_key_in = 0;
+}
+
+void lcd_menu(void){
+  adc_key_in =analogRead(0);
+  
+  switch (lcd_key){
+    case menu_ini:{
+      lcd.clear(0,0);
+      lcd.clear(0,1);
+      lcd.setCursor(0,0);
+      lcd.print("Set FiO2/Introduzca FiO2: left  ");
+      lcd.setCursor(0,1);
+      lcd.print("Set inlet Pressure/Introduzca presión de entrada : up");
+      lcd.setCursor(0,2);
+      lcd.print("Set tidal Volume/Introduzca Volumen tidal: right  ");
+      lcd.setCursor(0,3);
+      lcd.print("Set breathing rate/Introduzca frecuencia respiratoria : down  ");
+      lcd.setCursor(0,4);
+      lcd.print("Set I:E ratio/Introduzca tasa I:E : select  ");
+      if (adc_key_in < 50){
+        lcd_key = btnRIGHT;
+      }
+      else if (adc_key_in < 250){
+        lcd_key = btnUP;
+      }
+      else if (adc_key_in < 450){
+        lcd_key = btnDOWN;
+      }
+      else if (adc_key_in < 650){
+        lcd_key = btnLEFT;
+      }
+      else if (adc_key_in < 850){
+        lcd_key = btnSELECT;
+      }
+      else {
+        lcd_key = btnNONE;
+      }
+    }
+
+    case btnRIGHT:{
+      
+      break;
+    }
+
+    
+    case btnLEFT:{
+      
+      break;
+    }
+
+    
+    case btnUP:{
+      
+      break;
+    }
+
+    
+    case btnDOWN:{
+      
+      break;
+    }
+
+    
+    case btnSELECT:{
+      
+      break;
+    }
+
+    
+    case btnNONE:{
+      
+      break;
+    }
+  }
+}
---
+++ FrontEnd/FrontEnd/lcd.h
@@ -0,0 +1,8 @@
+
+#ifndef LCD_H_H
+#define LCD_H_H
+
+void init_lcd(void);
+void lcd_menu(void);
+
+#endif LCD_H_H
