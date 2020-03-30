3bc53e35318abf2f267ca4ecc0f0c66113a36e8e
mgmate123 <mgcmateo1234@gmail.com>
2020-03-30 09:04:33 -0500

    sketch to test drrvivers and comunication files

--- BackEnd/Driver_docs/drivers/drivers.ino
+++ BackEnd/Driver_docs/drivers/drivers.ino
@@ -1,9 +1,41 @@
+
+
+#include "easydriver.h"
+/**********************************************/
+//easy driver
+/*
 void setup() {
-  // put your setup code here, to run once:
+  configure_driver_pins(2,3);
+  motor_steps_per_rev(200);
+  initialize_driver_pins(2);
+  set_speed_motor(50);
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+ 
+ stepper_motor_action(1,true);
+ 
+ 
+}*/
 
+
+
+/**********************************************/
+//i2c master
+
+void setup() {
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+
+ 
+ 
 }
