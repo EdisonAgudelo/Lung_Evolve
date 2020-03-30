6c9a3ea81002406619225cb43b5f3ee04e62b40b
mgmate123 <mgcmateo1234@gmail.com>
2020-03-30 09:08:03 -0500

    simple gpio functions

---
+++ BackEnd/Driver_docs/drivers/gpio.h
@@ -0,0 +1,12 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include <Arduino.h>
+#define STATE_PIN 
+void set_GPIO_High(uint8_t PIN);
+void set_GPIO_Low(uint8_t PIN);
+bool state_GPIO(uint8_t PIN);
+
+
+
+#endif /*GPIO_H*/
