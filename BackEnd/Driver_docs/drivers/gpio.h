86aed37917af6dd47692d77f64cb471db8d78f55
Edison Agudelo <edison.1998@hotmail.es>
2020-04-06 01:02:04 -0500

    little change to standardise gpio interface

--- BackEnd/Driver_docs/drivers/gpio.h
+++ BackEnd/Driver_docs/drivers/gpio.h
@@ -2,6 +2,7 @@
 #define GPIO_H
 
 #include <Arduino.h>
+#include <stdint.h>
 
 /***************************/
 //ENDSTOP SWITCHES PIN CONFIGURATION
@@ -9,8 +10,8 @@
 #define ENDSTOP2 9
 #define ENDSTOP3 8
 
-void set_GPIO_High(uint8_t PIN);
-void set_GPIO_Low(uint8_t PIN);
-bool state_GPIO(uint8_t PIN);
+void set_pin(uint8_t pin);
+void clear_pin(uint8_t pin);
+bool read_digital_pin(int pin);
 
 #endif /*GPIO_H*/
