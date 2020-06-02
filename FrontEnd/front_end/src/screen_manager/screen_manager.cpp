2764ca47dae55801b40158b5cfd67d2fd9860f53
mgmate123 <mgcmateo1234@gmail.com>
2020-06-02 15:48:47 -0500

    just a // in the code

--- FrontEnd/front_end/src/screen_manager/screen_manager.cpp
+++ FrontEnd/front_end/src/screen_manager/screen_manager.cpp
@@ -433,11 +433,11 @@ void screen_management(void)
        
      }
      
-    Serial.print("n3.val=");  // This is sent to the nextion display to set what object name (before the dot) and what atribute (after the dot) are you going to change.
+    //Serial.print("n3.val=");  // This is sent to the nextion display to set what object name (before the dot) and what atribute (after the dot) are you going to change.
     //Serial.print(counter);  // This is the value you want to send to that object and atribute mentioned before.
-    Serial.write(0xff);  // We always have to send this three lines after each command sent to the nextion display.
-    Serial.write(0xff);
-    Serial.write(0xff);
+    //Serial.write(0xff);  // We always have to send this three lines after each command sent to the nextion display.
+    //Serial.write(0xff);
+    //Serial.write(0xff);
    }
 
 /*
