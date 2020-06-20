69a7359e8c28e18f9a6d2a8dfcade8abd74d80bb
EdisonAgudelo <edison.1998@hotmail.es>
2020-06-20 01:11:11 -0500

    Add flow sensor demo. New no blocking i2c was found and implemented. TODO: put the same library on backend

---
+++ BackEnd/pruebas/sfm3300/src/crc8.h
@@ -0,0 +1,31 @@
+/*
+    Lung Evolve Mechanical Ventilator
+    Copyright (C) 2020  Edison Agudelo, Mateo Garcia, Alejandra Londoño
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+    
+    */
+   
+#ifndef _CRC_8_H_
+#define _CRC_8_H_
+
+uint8_t CRC8Calculate(uint8_t *data, uint8_t length);
+
+//for single uint16_nummbers
+uint8_t CRC8Calculate(uint16_t data);
+
+uint8_t CRC8Configure(uint8_t poly, uint8_t initial);
+
+
+#endif
